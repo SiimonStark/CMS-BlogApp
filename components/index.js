c629d6d96179c417ee0085ede6e8b1306ee8035b
SiimonStark <siimonstark@gmail.com>
2021-11-11 18:27:46 -0800

    Add new Component files. WIP

--- components/index.js
+++ components/index.js
@@ -1,5 +1,9 @@
 export { default as PostCard } from './PostCard';
 export { default as Categories } from './Categories';
 export { default as PostWidget } from './PostWidget';
+export { default as PostDetail } from './PostDetail';
+export { default as CommentsForm } from './CommentsForm';
+export { default as Comment } from './Comment';
+export { default as Author } from './Author';
 export { default as Header } from './Header';
 export { default as Layout } from './Layout';
